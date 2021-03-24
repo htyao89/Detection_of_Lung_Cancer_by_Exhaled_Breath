cb72e27ef9bafcb90cdcb04171b55540dadfebac
yaohanzhigu <hantao.yao@ia.ac.cn>
2021-03-23 18:55:15 -0800

    Update main_final.m

--- main_final.m
+++ main_final.m
@@ -35,28 +35,4 @@ prec_rec(test_pred_score,1-test_label,'plotPR',1,'plotROC',0);
 figure;
 auc = roc_curve(test_pred_score,-test_label,0);
 
-%%
-% How many trees do you want in the forest? 
-nTrees = 50;
-% Train the TreeBagger (Decision Forest).
-B = TreeBagger(nTrees,train_data,train_label, 'Method', 'classification');
-predChar = B.predict(test_data);
-predictedClass = str2double(predChar);
-stats=confusionmatStats(test_label,predictedClass);
-cMat = stats.confusionMat;
-acc = (cMat(1,1)+cMat(2,2))/length(test_label);
-score=[acc2,stats2.sensitivity(1),stats2.specificity(1);acc,stats.sensitivity(1),stats.specificity(1)]
-
-% figure;
-% no_dims=2;
-% initial_dims=8;
-% perplexity=30;
-% mappedX = tsne(test_data,'Standardize',false);
-% gscatter(mappedX(:,1),mappedX(:,2),test_label);
-% 
-% figure;
-% weight = model.w; 
-% data = test_data.*weight;
-% mappedX = tsne(data,'Standardize',false);
-% gscatter(mappedX(:,1),mappedX(:,2),test_label);
 
